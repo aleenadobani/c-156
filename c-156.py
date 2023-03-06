d42cad16b9054304a942c6964d371dc3ed64b5bb
aleenadobani <98019373+aleenadobani@users.noreply.github.com>
2023-03-06 17:02:01 +0530

    Add files via upload

---
+++ c-156.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 16:27:46 2023
+
+@author: User
+"""
+
+from tkinter import *
+from PIL import ImageTk, Image
+root=Tk()
+
+root.title("Endless Dice Game")
+root.geometry("600x400")
+
+root.configure(background="yellow2")
+
+img=ImageTk.PhotoImage(Image.open ("dice1.4.jpg"))
+ 
+player1 = Label(root, text = " player 1", bg = "royal blue", fg = "white")
+player1.place(relx = 0.1, rely = 0.3 , anchor = CENTER)
+
+player2 = Label(root, text = " player 2", bg = "royal blue", fg = "white")
+player2.place(relx = 0.9, rely = 0.3 , anchor = CENTER)
+
+player_1_score_label = label(root , bg = "royal blue", fg = "white")
+player_1_score_label.place(relx = 0.1, rely = 0.4 , anchor = CENTER)
+
+player_2_score_label = label(root , bg = "royal blue", fg = "white")
+player_2_score_label.place(relx = 0.9, rely = 0.4 , anchor = CENTER)
+
+random_dice_label = Label(root,bg = "chocolate1", fg = "white")
+random_dice_label.place(relx = 0.5, rely = 0.5 , anchor = CENTER)
+
+root.mainloop()
